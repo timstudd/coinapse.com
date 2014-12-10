1b823f09ca79bd98013832b0f066960ce0577b32
Tim Studd <tim@timstudd.com>
2014-12-09 18:23:33 -0800

    Debug

--- web.js
+++ web.js
@@ -23,7 +23,7 @@ require('./register_models')();
 
 // app.use(express.compress());
 // app.use(logfmt.requestLogger());
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/templates/index.html');
