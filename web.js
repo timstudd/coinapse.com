0c0e6a3ce391524caf8010aa883a57c8b338bd12
Tim Studd <tim@timstudd.com>
2014-12-09 18:29:41 -0800

    Debug

--- web.js
+++ web.js
@@ -21,8 +21,8 @@ mongoose.connect(process.env.MONGOLAB_URI, function(err) {
 
 require('./register_models')();
 
-app.use(express.compress());
-app.use(logfmt.requestLogger());
+// app.use(express.compress());
+// app.use(logfmt.requestLogger());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
